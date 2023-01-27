43ecbc463a8438e26d1e14cb74c5062327894e8d
Camila Campos <camilacarta03@gmail.com>
2023-01-27 15:27:00 -0300

    atualização do libord e do main
    :wq

--- trab alg2/libOrd.h
+++ trab alg2/libOrd.h
@@ -2,11 +2,22 @@
 #define LIBORDENACAO_H
 
 void troca(int *vetor, int i, int j);
-int* insereOrdenado(int *vetor, int tam, int valor, int* numComparacoes);
 void imprimeVetor(int *vetor, int tam);
 int minimoVetor(int *vetor, int a, int b);
+
+int* insereOrdenado(int *vetor, int tam, int valor, int* numComparacoes);
+
 int selecionaEOrdena(int *vetor, int prim, int tam);
+
 void ordenaMerge(int vetor[], int ini, int tam);
 int* intercala(int *vetor, int ini, int meio, int tam);
+
+int particiona(int vetor[], int tam, int pivo);
+void ordenaQuick(int vetor[], int ini, int tam);
+
+void maxHeapify(int vetor[], int tam, int ini);
+void buildMaxHeapify(int vetor[], int tam);
+void ordenaHeap(int vetor[], int tam);
+
 
 #endif
--- trab alg2/main.c
+++ trab alg2/main.c
@@ -3,6 +3,18 @@
 #include <time.h>
 
 #include "ordenacao.h"
+#include "libOrd.h"
+
+#define TAM_VETOR 10000 /* tamanho do vetor */
+#define MAX_VALOR 32768 /* 2^15; valor max q um numero pode assumir */
+
+int* vetorRand(int* vetor){
+	int i;
+	for (int i = 0; i < TAM_VETOR; ++i){
+		vetor[i] = rand() % MAX_VALOR;
+	}
+	return vetor;
+}
 
 int main(){
 	char nome[MAX_CHAR_NOME];
@@ -13,45 +25,52 @@ int main(){
 
 	//Dica: somente é posśivel criar vetores grandes utilizando alocação dinâmica de memória
 	//Veja um exemplo de alocação dinâmica a seguir
-	int tamVetor = 3;
-	int* vetor = malloc(tamVetor * sizeof(int));
+	int* vetor = malloc(TAM * sizeof(int));
 	if(vetor == NULL){
 		printf("Falha fatal. Impossível alocar memoria.");
-		return 1;
+		return -1;
 	}
 	//Depois de alocado, o vetor pode ser utilizado normalmente
 	//Não esqueça de desalocar no final do programa via free
-		
-	vetor[0] = 1;
-	vetor[1] = 10;
-	vetor[2] = 12;
+
 
 	getNome(nome);
 	getNome(nome2);
 	printf("Trabalho de %s e %s \n", nome, nome2);
 	printf("GRR %u, GRR %u \n", getGRR1(), getGRR2());
 
-	getNome(nome2);
+/*	getNome(nome2);
 	printf("Trabalho de %s\n", nome2);
-	printf("GRR %u\n", getGRR());
+	printf("GRR %u\n", getGRR());*/
 
-	//Para medir o tempo, inclua time.h, e siga o exemplo:
+	printf("Criando vetor de tamanho %d para o Insertion Sort.\n", TAM_VETOR);
+	vetor = vetorRand(vetor);
 	clock_t start, end;//variáveis do tipo clock_t
     double total;
-
 	start = clock();//start recebe o "ciclo" corrente
-	numComp = insertionSort(vetor, 3);
+	numComp = insertionSort(vetor, TAM_VETOR);
 	end = clock();//end recebe o "ciclo" corrente
 	//o tempo total é a diferença dividia pelos ciclos por segundo
 	total = ((double)end - start)/CLOCKS_PER_SEC;
 	printf("Tempo total: %f", total);
 
-	//*numComparacoes = 0;
+	*numComparacoes = 0;
 
-	numComp = selectionSort(vetor, 3);
-	numComp = mergeSort(vetor, 3);
-	numComp = quickSort(vetor, 3);
-	numComp = heapSort(vetor, 3);
+	printf("Criando vetor de tamanho %d para o Insertion Sort.\n", TAM_VETOR);
+	vetor = vetorRand(vetor);
+	clock_t start, end;//variáveis do tipo clock_t
+    double total;
+	start = clock();//start recebe o "ciclo" corrente
+	numComp = selectionSort(vetor, TAM_VETOR);
+
+
+	numComp = mergeSort(vetor, TAM_VETOR);
+
+
+	numComp = quickSort(vetor, TAM_VETOR);
+	
+
+	numComp = heapSort(vetor, TAM_VETOR);
 
 	for(int i=0; i < 3; i++){
 		printf("%d ", vetor[i]);
